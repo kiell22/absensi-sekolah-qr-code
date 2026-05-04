4e08e71a106eb645f4699d8e8d999cf03d00dcd3
kiell22 <kiell22@github.com>
2026-05-04 22:23:00 +0900

    Update GitHub link in footer to kiell22

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+   <div class="container-fluid">
+      <nav class="float-left">
+         <ul>
+            <li>
+               <a href="https://github.com/kiell22/absensi-sekolah-qr-code">
+                  Github
+               </a>
+            </li>
+         </ul>
+      </nav>
+      <div class="copyright float-right">
+         <?= $generalSettings->copyright; ?>
+      </div>
+   </div>
+</footer>
